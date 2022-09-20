134f30dacbe0b301b0de1b4ce2f753bf110f6938
Дмитрий Малашенко <dimaneopal@mail.ru>
2022-09-20 18:44:52 +0300

    +2

--- Laba№1/1 level/Num 1.cs
+++ Laba№1/1 level/Num 1.cs
@@ -4,14 +4,14 @@ namespace Laba_1
 {
     class Program
     {
-        //static void Main(string[] args)
-        //{ 
-        //    int s = 0;
-        //    for(int k =2; k<=35; k = k+3)
-        //    {
-        //     s = s + k;
-        //    }
-        //    Console.WriteLine(s);
-        //}
+        static void Main(string[] args)
+        {
+            int s = 0;
+            for (int k = 2; k <= 35; k = k + 3)
+            {
+                s = s + k;
+            }
+            Console.WriteLine(s+2);
+        }
     }
 }
