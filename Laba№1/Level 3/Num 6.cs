ed6bdffd1b92578fdc6c2de2daef2164f333b897
Дмитрий Малашенко <dimaneopal@mail.ru>
2022-09-21 14:14:52 +0300

    второй

--- Laba№1/Level 3/Num 6.cs
+++ Laba№1/Level 3/Num 6.cs
@@ -1,33 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Diagnostics.CodeAnalysis;
+//using System.Text;
 
-namespace Laba_1.Level_3
-{
-    class Num_6
-    {
-        //static void Main(string[] args)
-        //{
-        //    double lim = 0.0001;
-        //    double i = 1;
-        //    double s = 0;
+//namespace Laba_1.Level_3
+//{
+//    class Num_6
+//    {
+//        static void Main(string[] args)
+//        {
+//            double lim = 0.0001;
+//            double sum = 0;
+//            double s = 0;
 
-        //    double y = 0;
-        //    for (double x = 0.1; x <= 1; x += 0.1)
-        //    {
-        //        if (s <= 0.0001)
-        //        {
+//            double y = 0;
+//            for (double x = 0.1; x <= 1; x += 0.1)
+//            {
+//                for (int i = 1; i <10; i++)
+//                {
 
-        //            s = (Math.Pow(-1, i + 1)) * (Math.Pow(x, 2 * i + 1)) / (4 * i * i - 1) + s;
+//                while  (y <= 0.0001)
+//                {
 
-        //            y = ((1 + x * x) * Math.Atan(x)) / (2) - (x / 2);
+//                    s = (Math.Pow(-1, i + 1)) * (Math.Pow(x, 2 * i + 1)) / (4 * i * i - 1) ;
 
-        //            Console.WriteLine("при x=" + x + " s=" + s + " y=" + y);
+//                    y = ((1 + x * x) * Math.Atan(x)) /2 - (x / 2);
 
-        //        }
-        //    }
+//                    sum += s;    
+//                    Console.WriteLine("при x=" + x + " sum=" + sum + " y=" + y);
+
+//                }
+
+//                }
+//            }
 
 
-        //}
-    }
-}
+//        }
+//    }
+//}
