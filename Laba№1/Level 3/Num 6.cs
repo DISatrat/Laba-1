8a546ed983646f022657f940c344b19a1c1f7545
Дмитрий Малашенко <dimaneopal@mail.ru>
2022-09-21 21:24:35 +0300

    3 уровень

--- Laba№1/Level 3/Num 6.cs
+++ Laba№1/Level 3/Num 6.cs
@@ -9,29 +9,28 @@
 //    {
 //        static void Main(string[] args)
 //        {
-//            double lim = 0.0001;
-//            double sum = 0;
-//            double s = 0;
 
-//            double y = 0;
-//            for (double x = 0.1; x <= 1; x += 0.1)
+//            for (double k = 1; k <= 10; k += 1)
 //            {
-//                for (int i = 1; i < 10; i++)
+//                double x = k / 10;
+//                double sum = 0;
+//                double i = 1;
+
+//                while (true)
 //                {
+//                    double s = 0;
+//                    s = (Math.Pow(-1, i + 1)) * (Math.Pow(x, 2 * i + 1)) / (4 * Math.Pow(i, 2) - 1);
 
-//                    while (y <= 0.0001)
+//                    if (Math.Abs(s) < 0.0001)
 //                    {
-
-//                        s = (Math.Pow(-1, i + 1)) * (Math.Pow(x, 2 * i + 1)) / (4 * i * i - 1);
-
-//                        y = ((1 + x * x) * Math.Atan(x)) / 2 - (x / 2);
-
-//                        sum += s;
-//                        Console.WriteLine("при x=" + x + " sum=" + sum + " y=" + y);
-
+//                        break;
 //                    }
-
+//                    sum += s;
+//                    i++;
 //                }
+
+//                double y = (1 + Math.Pow(x, 2)) * Math.Atan(x) / 2 - (x / 2);
+//                Console.WriteLine("При x = " + x + "; sum = " + sum + "; y = " + y);
 //            }
 
 
